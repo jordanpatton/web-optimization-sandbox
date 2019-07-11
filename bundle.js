84f654365c1ab9fedaabba4c25f9e95eb1f7408c
Jordan Patton <jordanpatton@gmail.com>
2019-07-11 16:47:24 -0700

    workaround

--- bundle.js
+++ bundle.js
@@ -1,7 +1,15 @@
 (function() {
-    console.log('bundle.js');
     if (typeof window !== 'undefined' && typeof window.Worker !== 'undefined') {
-        var bundleWorker = new Worker('bundle.worker.js');
+        // https://stackoverflow.com/questions/21408510/chrome-cant-load-web-worker
+        var bundleWorker = new Worker(
+            URL.createObjectURL(
+                new Blob(
+                    ['('+bundleWorkerJs.toString()+')()'],
+                    {type: 'text/javascript'}
+                )
+            )
+        );
+
         var bundleWorkerButton = document.getElementById('bundleWorkerButton');
         bundleWorkerButton.onclick = function (_event) {
             console.log('click', bundleWorker);
--- bundle.worker.js
+++ bundle.worker.js
@@ -1,8 +1,12 @@
-if (typeof postMessage !== 'undefined') {
-    console.log('bundle.worker.js');
+// https://stackoverflow.com/questions/21408510/chrome-cant-load-web-worker
+function bundleWorkerJs() {
     onmessage = function (_event) {
         const data = Date.now();
-        console.log('worker.js:onmessage', data);
+        console.log('bundleWorkerJs:onmessage', data);
         postMessage(data);
     };
+}
+
+if (self !== window) {
+    bundleWorkerJs();
 }
