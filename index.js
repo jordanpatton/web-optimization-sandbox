ef0bccc4c7f47044e013ff67b9408ac095e704a9
Jordan Patton <jordanpatton@gmail.com>
2019-07-11 17:50:04 -0700

    remove 20-user limit

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ app.get('/', (_req, res, _next) => res.send('OK'));
 app.get('/api', (_req, res, _next) => res.json({}));
 
 // users routes
-app.get('/api/users', (req, res, _next) =>
-    req.query.userId
-    ? res.json({users: userFixtures.filter(val => String(val.id) === String(req.query.userId))})
-    : res.json({users: userFixtures.slice(0, 19)})
-);
+app.get('/api/users', (_req, res, _next) => res.json({users: userFixtures}));
 
 // 404
 app.get('*', (_req, res, _next) => res.status(404).send('Not Found'));
