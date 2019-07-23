09688901d7e88aea6a69008490cc4c5b64e606c4
Jordan Patton <jordanpatton@gmail.com>
2019-07-23 15:26:24 -0700

    comments

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,9 @@ import ReduxApp from './redux-app/components/App';
 export const Root = () => <ReduxApp />;
 
 if (getRenderMode() === RENDER_MODE_CONCURRENT) {
-    // render mode: concurrent (https://reactjs.org/blog/2018/11/27/react-16-roadmap.html)
+    // render mode: concurrent
+    // -- https://reactjs.org/blog/2018/11/27/react-16-roadmap.html
+    // -- https://developers.google.com/web/updates/2015/08/using-requestidlecallback
     (ReactDOM as any).unstable_createRoot(document.getElementById('root')).render(<Root />);
 } else {
     // render mode: normal
